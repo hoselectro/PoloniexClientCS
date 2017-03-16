ace65a5c2d0b7c019f53cb8b579ee0ecc0f56cc2
mmatthew <takeknock@gmail.com>
2017-03-17 00:49:35 +0900

    Added constructor in PoloniexClient

--- poloniex/PoloniexClient.cs
+++ poloniex/PoloniexClient.cs
@@ -8,5 +8,13 @@ namespace poloniex
 {
     class PoloniexClient
     {
+        private string apiKey;
+        private string secret;
+
+        public PoloniexClient(string apiKey, string secret)
+        {
+            this.apiKey = apiKey;
+            this.secret = secret;
+        }
     }
 }
